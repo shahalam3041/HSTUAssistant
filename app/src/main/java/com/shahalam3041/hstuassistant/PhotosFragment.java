07c96de0fd690087d95adc42c46b7a37bdcec6fc
ShahAlam <shahalam3041@gmail.com>
2018-01-18 23:45:14 +0600

    Initial commit Listener to the fragments

---
+++ app/src/main/java/com/shahalam3041/hstuassistant/PhotosFragment.java
@@ -0,0 +1,20 @@
+package com.shahalam3041.hstuassistant;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class PhotosFragment extends android.app.Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_photos, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/shahalam3041/hstuassistant/RoutineFragment.java
@@ -0,0 +1,21 @@
+package com.shahalam3041.hstuassistant;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RoutineFragment extends android.app.Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_routine, container, false);
+    }
+
+}
